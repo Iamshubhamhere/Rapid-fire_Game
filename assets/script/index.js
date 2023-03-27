28faa29bd37ea81da77d875f593833f9a62e6e62
Shubham <shubham11729@gmail.com>
2023-03-26 19:44:34 -0500

    Updated

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const grid = document.querySelector('.grid');
-const timer = document.querySelector('.timer');
+const timer = document.querySelector('#timer');
 const current = document.querySelector('.current');
 const textInput = document.querySelector('.text-input');
 const StartBtn = document.querySelector('.start');
 const resetBtn = document.querySelector('.reset');
 const hardMode = document.querySelector('.hardMode');
-const noOfHits = document.querySelector('.hits');
-const highScore = document.querySelector('.score');
-const message = document.querySelector('.message');
+const noOfHits = document.querySelector('#hits');
+const highScore = document.querySelector('#score');
+const message = document.querySelector('h2');
 
 const BgSound = new Audio('./assets/audio/backgroundSound.mp3');
 BgSound.type = 'audio/mp3';
@@ -20,7 +20,9 @@ hardModeSound.volume = 0.4;
 const hitSound = new Audio('./assets/audio/hitSound.mp3');
 hitSound.type = 'audio/mp3';
 hitSound.volume = 0.3;
-
+const wrongSound = new Audio('./assets/audio/wrongAnswer.mp3');
+wrongSound.type = 'audio/mp3';
+wrongSound.volume = 0.3;
 
 const words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 
 'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute',
@@ -58,7 +60,7 @@ class Score {
       };
       
       getScore() {
-        return `${this.#date} | Hits: ${this.#hits} | ${this.#percentage.toFixed(2)}%`;
+        return `${this.#date} | Hits: ${this.#hits} | Percentage: ${this.#percentage.toFixed(2)}%`;
       };
 }
 
@@ -67,42 +69,68 @@ let hits = 0;
 let perc;
 let date = new Date().toDateString().slice(3, 10);
 const player = new Score(date, hits, perc);
+let time = 99;
+let points = 0 ;
+let gameRunning = false;
+//Update time
 
-function countdownTimer() {
-  let countdown = 99;
-  let countdownInterval = setInterval(() => {
-    countdown--;
-    timer.innerText = `Timer: ${countdown} Seconds`;
+function updateTime() {
+  if (!gameRunning) {
+    return;
+  }
+  timer.innerHTML = `${time}s`;
+    if(time > 0){
+    // decrement
+    time--;
     
-    // will stop before typing the last word
-    // if(countdown === 0 || words.length === 0) {
-    if(countdown === 0) {
-      BgSound.pause();
-      
-      clearInterval(countdownInterval);
-      
-   
-      player.getPercentage();
-      highScore.innerText = player.getScore();
-    }
-  }, 1000);
+  } 
+
+  else if (time === 0) {
+    BgSound.pause();
+  //console.log(time);
+  }
 }
 
 
+
+
+
 function randomWord(words) {
-    let random = words[Math.floor(Math.random() * words.length)];
+    const random = words[Math.floor(Math.random() * words.length)];
     return random;
   }
 
+function getRandomWord() {
+    let wordSelected = words[Math.floor(Math.random() * words.length)];
+   
+    //Delete word selected of the array
+    const indexToRemove = words.indexOf(wordSelected);
+      if (indexToRemove !== -1) {
+      words.splice(indexToRemove, 1);
+    }
+    return wordSelected;
+  }
+  
+  //Display random word in the screen
+  function displayRandomWord() {
+    randomWord = getRandomWord();
+    current.innerHTML = randomWord;
+  }
 
   StartBtn.addEventListener('click', () => {
+   
+    gameRunning = true;
+    setInterval(updateTime, 1000);
+
+    setTimeout(() => {
+      setInterval();
+    }, 99000);
+    textInput.style.visibility = 'visible';
     BgSound.play();
     
  
     
     textInput.focus();
-    
-    countdownTimer();
     
     let random = randomWord(words);
     
@@ -111,5 +139,70 @@ function randomWord(words) {
     current.innerText = random;
     textInput.maxLength = random.length;
     
+    
   });
 
+  
+  textInput.addEventListener('input', e => {
+    const insertedText = e.target.value;
+  
+    if (insertedText === current.innerText) {
+      hitSound.play();
+      message.innerHTML = 'Correct!!';
+      points+=5;
+      hits++;
+      noOfHits.innerHTML = `${hits}`;
+      highScore.innerHTML = `${points}`;
+      if (points === 90) {
+        endGame();
+      }
+      displayRandomWord();
+      setTimeout(() => {
+        message.innerHTML = ' ';
+      }, 1000);
+    
+    } else {
+    
+    return false;
+    }
+    
+    e.target.value = '';
+    updateTime();
+  });
+  
+resetBtn.addEventListener('click', () => {
+  // Reset game state
+  hits = 0;
+  perc = 0;
+  points = 0;
+  time = 0;
+  
+  words.splice(0, words.length);
+  words.push(
+    'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 
+    'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute',
+    'discipline', 'machine', 'accurate', 'connection', 'rainbow', 'bicycle',
+    'eclipse', 'calculator', 'trouble', 'watermelon', 'developer', 'philosophy',
+    'database', 'periodic', 'capitalism', 'abominable', 'component', 'future',
+    'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty', 'agency',
+    'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 'magician',
+    'professor', 'triangle', 'earthquake', 'baseball', 'beyond', 'evolution',
+    'banana', 'perfumer', 'computer', 'management', 'discovery', 'ambition', 'music',
+    'eagle', 'crown', 'chess', 'laptop', 'bedroom', 'delivery', 'enemy', 'button',
+    'superman', 'library', 'unboxing', 'bookstore', 'language', 'homework',
+    'fantastic', 'economy', 'interview', 'awesome', 'challenge', 'science', 'mystery',
+    'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
+    'keyboard', 'window', 'population'
+  );
+  gameRunning = false;
+  BgSound.pause();
+  BgSound.currentTime = 0;
+  noOfHits.innerHTML = `${hits}`;
+  highScore.innerHTML = `${points}`;
+  timer.innerHTML = `${time}s`;
+  textInput.value = '';
+  textInput.style.visibility = 'visible';
+  current.value = '';
+  current.style.visibility = 'visible';
+  message.innerHTML = ' ';
+});
